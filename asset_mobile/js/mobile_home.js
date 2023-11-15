54259b1a36185415ff4b31172cc30a0ecc7204d8
taher2552 <stardom.taher@gmail.com>
2023-11-15 17:31:58 +0530

    changes

--- asset_mobile/js/mobile_home.js
+++ asset_mobile/js/mobile_home.js
@@ -278,7 +278,7 @@ window.addEventListener("resize", detectMobile);
 
 // Adjust the modal position on focus/blur of inputs
 document
-  .querySelectorAll("#mob_new_name, .mob_phone, .mob_desc")
+  .querySelectorAll(".mob_phone, .mob_desc")
   .forEach(function (input) {
     input.addEventListener("focus", function () {
       adjustModalPosition();
